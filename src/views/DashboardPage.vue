3892d0726bebc35bfe1ca3e8738dd37313491202
Carlos Henrique Benício Costa <ique.benicio@hotmail.com>
2024-04-12 21:08:26 -0300

    update
    atualização sobre a tela do Dashboard

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,10 +1,17 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script lang="ts">
-export default {
-	props: {
+//import Vue from 'vue';
+//import Youtube from 'vue-youtube';
+
+//Vue.use(Youtube);
+
+export default {	
+	components: {
+//		Youtube
 	},
 	data() {
 		return {
+			videoId: 'https://youtu.be/ItFi-eomRI8?si=fO1l5LArikf_DmVi'
 		};
 	},
 	methods: {
@@ -15,19 +22,89 @@ export default {
 <template>
 	<div>
 		<div class="center">
-			<div class="box">
-				<label class="center" for="">Quantas pessoas atualmente</label>
+			<div class="box1">
+				<h3 class="center">Monitoramento</h3>
+				<table class="center">
+					<tr>
+						<th>Cam ID</th>
+						<th>Quantidade</th>
+						<th>Status</th>
+						<th>Horario</th>
+						<th>Observação</th>
+
+					</tr>
+					<tr>
+						<td>Nome da Sala</td>
+						<td>10 Pessoas</td>
+						<td class="red">Saida</td>
+						<td>13:00</td>
+						<td></td>					
+					</tr>
+					<tr>
+						<td>Nome da Sala</td>
+						<td>10 Pessoas</td>
+						<td class="green">Entrada</td>
+						<td>11:00</td>
+						<td></td>				
+					</tr>
+					<tr>
+						<td>Nome da Sala</td>
+						<td>03 Pessoas</td>
+						<td class="red">Saida</td>
+						<td>7:00</td>
+						<td></td>				
+					</tr>
+					<tr>
+						<td>Nome da Sala</td>
+						<td>02 Pessoas</td>
+						<td class="green">Entrada</td>
+						<td>6:00</td>
+						<td></td>				
+					</tr>
+
+				</table>
+				<div>
+				</div>
 			</div>
-			<div class="box">
-				<label class="center" for="">Horario das pessoas de hoje </label>
+			<div class="box2">
+				<table class="center">
+					<br>
+					<br>
+					<tr>
+						<th>Sala</th>
+						<th>Movimentação do Dia</th>
+					</tr>
+					<tr>
+						<td>Nome da Sala</td>
+						<td>10</td>				
+					</tr>
+					<tr>
+						<td>Nome da Sala</td>
+						<td>5</td>				
+					</tr>
+					<tr>
+						<td>Nome da Sala</td>
+						<td>3</td>				
+					</tr>
+					<tr>
+						<td>Nome da Sala</td>
+						<td>2</td>				
+					</tr>
+				</table>
 			</div>
 		</div>
 		<div class="center">
-			<div class="box">
-				<label class="center" for="">Registro de pessoas e seu horario.</label>
+			<div class="box2">
+				<br>
+				<br>
+				<h3 class="center">Pessoas na RedZone</h3>
+				<label for="" class="center padding">
+					<p>0</p>
+				</label>
 			</div>
-			<div class="box">
-				<label class="center" for="">Tela de video</label>
+			<div class="box1">
+				<label class="center" for="">Tela de monitoramento em tempo real</label>
+				<youtube :video-id="videoId" :autoplay="true" width="600"></youtube>
 			</div>
 		</div>
 	</div>
@@ -35,6 +112,33 @@ export default {
 
 <style scoped>
 
+.padding{
+	font-size: 50px;
+}
+
+.red{
+ background-color: #E63349;
+ color: white;
+}
+.green{
+background-color: #00CA72;
+color: white;
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: center;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
 .space{
 margin-right: 101px;
 }
@@ -68,39 +172,28 @@ text-align:start;
 border: 1px solid #ccc;
 }
 
-.button1{
-color: white;
-width: 91px;
-height: 29px;
-background-color: #E63349;
-border-radius: 5px;
-}
-
-.button2{
-color: white;
-width: 91px;
-height: 29px;
-background-color: #1DA4FB;
-border-radius: 5px;
-}
-
-.button3{
-color: white;
-width: 91px;
-height: 29px;
-background-color: #00CA72;
-border-radius: 5px;
-}
-
-.box{ 
+.box1{ 
 margin: 20px;
 align-items: center;
 justify-content: center;
 display: inline-block;
 border-color: #000;
 background-color: white;
-height: 250px;
-width: 500px;
+height: 260px;
+width: 540px;
+color: #000;
+border-radius: 20px;
+border: 2px solid gray;
+}
+.box2{ 
+margin: 20px;
+align-items: center;
+justify-content: center;
+display: inline-block;
+border-color: #000;
+background-color: white;
+height: 260px;
+width: 350px;
 color: #000;
 border-radius: 20px;
 border: 2px solid gray;
