2eee9327f2af8c9b72d3d049a3b853c0223a60ee
Thamires de Souza Barbosa <thamiressouza2000@gmail.com>
2024-05-04 20:38:33 -0300

    fix - get-entrada-saida - [TGN-11]
    Fix - Correção de código de chamada de dados do back-end para o front-end
    
    Correção do código da parte de monitoramento, ainda vou revisar o código da parte de movimentação, e fazer as devidas correções, se necessárias
    
    Thamires - status conclusão - [TGN - 11]

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path:'/registroEntrada',
+      path: '/registroEntrada',
       name: 'registroEntrada',
       component: () => import('@/views/RegistroEntrada.vue')
     },
     {
-      path:'/registroSaida',
+      path: '/registroSaida',
       name: 'registroSaida',
       component: () => import('@/views/RegistroSaida.vue')
     },
--- src/views/RegistroSaida.vue
+++ src/views/RegistroSaida.vue
@@ -9,7 +9,9 @@ export default {
   <div class="sideMenu">
     <aside>
       <button type="button" class="button"><a href="/dashboard">Home</a></button>
-      <button type="button" class="button"><a href="/controle-acesso">Controle de acesso</a></button>
+      <button type="button" class="button">
+        <a href="/controle-acesso">Controle de acesso</a>
+      </button>
       <button type="button" class="button"><a href="/relatorios">Relatórios</a></button>
       <button type="button" class="button"><a href="/monitor-acesso">Monitor de acesso</a></button>
       <button type="button" class="button"><a href="/cadastro">Cadastros</a></button>
@@ -19,73 +21,72 @@ export default {
   </div>
   <div class="center">
     <div class="box1">
-        <h3 class="center">Edição de Registro</h3>
-        <br>
-        <div>
-            <label for="text" style="color: #FF0000;">Redzone</label>
-        </div>
-        <br>
-        <div>
-            <input type="text" placeholder="Redzone" class="input1">
-        </div>
-        <br>
-        <div>
-            <input type="text" placeholder="Data de Procura" class="input2">
-        </div>
-        <br>
-        <div class="container">
-            <label for="horario">Horario:</label>
-            <input type="text" id="horario" class="input2">
-        </div>
-        <br>
-        <div class="container">
-            <label for="quantidade">Quantidade de Pessoas:</label>
-            <input type="text" id="quantidade" class="input2">
-        </div>
-        <br><br><br>
-        <div class="center">
-          <RouterLink to="/registroEntrada">
-            <button @click="entrar" class="button1">Entrada</button>
-          </RouterLink>
-          <RouterLink to="/registroSaida">
-            <button @click="entrar" class="button2">Saida</button>
-          </RouterLink>
-            <button class=" button3">Confirmar</button>
-            <button class="button4">Excluir</button>
-        </div>
-
+      <h3 class="center">Edição de Registro</h3>
+      <br />
+      <div>
+        <label for="text" style="color: #ff0000">Redzone</label>
+      </div>
+      <br />
+      <div>
+        <input type="text" placeholder="Redzone" class="input1" />
+      </div>
+      <br />
+      <div>
+        <input type="text" placeholder="Data de Procura" class="input2" />
+      </div>
+      <br />
+      <div class="container">
+        <label for="horario">Horario:</label>
+        <input type="text" id="horario" class="input2" />
+      </div>
+      <br />
+      <div class="container">
+        <label for="quantidade">Quantidade de Pessoas:</label>
+        <input type="text" id="quantidade" class="input2" />
+      </div>
+      <br /><br /><br />
+      <div class="center">
+        <RouterLink to="/registroEntrada">
+          <button @click="entrar" class="button1">Entrada</button>
+        </RouterLink>
+        <RouterLink to="/registroSaida">
+          <button @click="entrar" class="button2">Saida</button>
+        </RouterLink>
+        <button class="button3">Confirmar</button>
+        <button class="button4">Excluir</button>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-    .container {
-        display: flex;
-        justify-content: space-between;
-        padding: 20px; 
-    }
-    .container label {
-        text-align: left;
-    }
+.container {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+}
+.container label {
+  text-align: left;
+}
 
-    .container input {
-        text-align: right
-    }
-.input1{
+.container input {
+  text-align: right;
+}
+.input1 {
   margin: 5px;
   width: 682px;
   height: 30px;
   font-size: 15px;
 }
-.input2{
+.input2 {
   text-align: center;
   margin: 5px;
   width: 100px;
   height: 25px;
   font-size: 15px;
 }
-.style{
-  align-items:center;
+.style {
+  align-items: center;
   display: flex;
   flex-direction: row;
 }
@@ -101,14 +102,14 @@ export default {
   color: #000;
   border-radius: 20px;
   border: 2px solid gray;
-  }
+}
 .center {
   margin: 5px;
   display: flex;
   justify-content: center;
   align-items: center;
 }
-.center1{
+.center1 {
   margin: 5px;
   display: flex;
   justify-content: start;
@@ -130,29 +131,19 @@ export default {
   font-size: 20px;
   border: none;
 }
-.button1{
+.button1 {
   margin: 5px;
   color: #000;
   border-radius: 15px;
-  background-color: #FCFCFC;
+  background-color: #fcfcfc;
   font-size: 20px;
   height: 47px;
   width: 150px;
   border-color: #000;
 }
-.button2{
+.button2 {
   margin: 5px;
-  background-color: #1DA4FB;
-  color: #000;
-  border-radius: 15px;
-  font-size: 20px;
-  height: 47px;
-  width: 150px;
-  border-color: #000;
-}
-.button3{
-  margin: 5px;
-  background-color: #01A84D;
+  background-color: #1da4fb;
   color: #000;
   border-radius: 15px;
   font-size: 20px;
@@ -160,9 +151,19 @@ export default {
   width: 150px;
   border-color: #000;
 }
-.button4{
+.button3 {
   margin: 5px;
-  background-color: #FF0000;
+  background-color: #01a84d;
+  color: #000;
+  border-radius: 15px;
+  font-size: 20px;
+  height: 47px;
+  width: 150px;
+  border-color: #000;
+}
+.button4 {
+  margin: 5px;
+  background-color: #ff0000;
   color: #000;
   border-radius: 15px;
   font-size: 20px;
